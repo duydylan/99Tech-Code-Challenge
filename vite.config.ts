1b5133d354bbcd7bf1a4bfa19d7b8bd435f39c40
Duong Minh Duy <duongminhduy@Duongs-MacBook-Air.local>
2025-01-20 23:30:01 +0700

    config deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import eslint from "vite-plugin-eslint";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), eslint()],
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
+            '@': path.resolve(__dirname, './src'),
         },
     },
+    base: '/99tech-code-challenge-frontend-duyduong',
 });
